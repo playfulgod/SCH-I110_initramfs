ae5d9b529544a4731941f54afb29343118207168
playfulgod <playfulgod@gmail.com>
2012-06-14 18:44:12 -0400

    changes for root

---
+++ init.superuser.sh
@@ -0,0 +1,9 @@
+#script by Da_G to set up su and busybox adapted for i927(utkanos) adapted to i110(PlayfulGod)
+mount -o rw,remount /dev/block/mmcblk0p8 /system
+/busybox cp /su /system/xbin/su
+/busybox cp /busybox /system/xbin/busybox
+/system/xbin/busybox --install -s /system/xbin
+/system/xbin/busybox chmod 4755 /system/xbin/su
+#init.d tweak - utkanos
+/system/xbin/busybox run-parts /system/etc/init.d/
+mount -o ro,remount /dev/block/mmcblk0p8 /system
